918c0775f4b959612cce9a249565106fb24a64de
MG-Trener <mihagavr@gmail.com>
2026-09-20 12:45:58 +0500

    feat(mobile): render persistent deposit navigation target

--- apps/mobile/src/IndustrialMapLayer.tsx
+++ apps/mobile/src/IndustrialMapLayer.tsx
@@ -1,13 +1,20 @@
 import type { ComponentProps } from 'react';
 import { BuildRadiusLayer } from './BuildRadiusLayer';
 import { IndustrialMapLayer as IndustrialMapLayerCore } from './IndustrialMapLayerCore';
+import { useNavigationTarget } from './navigationTarget';
 
 type Props = ComponentProps<typeof IndustrialMapLayerCore>;
 
 export function IndustrialMapLayer(props: Props) {
+  const navigationTarget = useNavigationTarget();
+
   return (
     <>
-      <BuildRadiusLayer selectedH3={props.selectedH3} />
+      <BuildRadiusLayer
+        selectedH3={props.selectedH3}
+        navigationTarget={navigationTarget}
+        onTargetPress={(h3Index) => props.onSelect?.(h3Index)}
+      />
       <IndustrialMapLayerCore {...props} />
     </>
   );
